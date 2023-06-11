6001d73fd03857bb9671221aec4aab663bdf5f0c
Alvin <103211949+gkalvarog@users.noreply.github.com>
2023-06-10 21:28:33 -0300

    finalizing adding htmlS

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Home from "./components/Home";
+import Users from "./components/Users";
+import UserBlog from "./components/UserBlog";
+import UserPostForm from "./components/UserPostForm";
 
 import "./styles/normalize.css";
 import "./styles/fontawesome.min.css";
 import "./styles/main.css";
 
 function App() {
-  return <Home />;
+  return <UserPostForm />;
 }
 
 export default App;
